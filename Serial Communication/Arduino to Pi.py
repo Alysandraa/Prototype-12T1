c7c665ea1bc8f2c6ff535f2c882c574db733e393
Alysanb <144813223+Alysanb@users.noreply.github.com>
2024-04-08 20:18:29 +1000

    Update Arduino to Pi.py

--- Serial Communication/Arduino to Pi.py
+++ Serial Communication/Arduino to Pi.py
@@ -1,9 +1,12 @@
 import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+#identifying the USB port for the arduino
+ser = serial.Serial('/dev/ttyACM0',9600)  
 s = [0,1]
 while True:
+    #have to decode string from the arduino
     read_serial=ser.readline().decode('utf-8')
     s[0] = str(read_serial)
+    #print the string from the arduino
     print (s[0])
     print (read_serial)
