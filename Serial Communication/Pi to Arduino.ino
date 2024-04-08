70fc172fff4c467de0df239d46b20c73c5e9f2da
Alysanb <144813223+Alysanb@users.noreply.github.com>
2024-04-08 20:28:15 +1000

    Update Pi to Arduino.ino

--- Serial Communication/Pi to Arduino.ino
+++ Serial Communication/Pi to Arduino.ino
@@ -1,13 +1,11 @@
 void setup(){
-   
-  // Set the baud rate  
+  // Starting serial communication
   Serial.begin(9600);
-   
 }
  
 void loop(){
- 
   if(Serial.available() > 0) {
+     //sets a parameter for when to stop reading the string
     int data = Serial.readStringUntil('\n');
     Serial.print("Hi Raspberry Pi! You sent me: ");
     Serial.println(data);
