453e555ad9d4114b0ffca45d5c8242f7ba96e2ca
Alysanb <144813223+Alysanb@users.noreply.github.com>
2024-04-08 20:29:39 +1000

    Update Pi to Arduino.py

--- Serial Communication/Pi to Arduino.py
+++ Serial Communication/Pi to Arduino.py
@@ -1,11 +1,13 @@
 import serial
 import time
 
+#identifying USB port for arduino
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.flush()
 while(1):
     send_string = ("Hi\n")
     ser.write(send_string.encode('utf-8'))
     time.sleep(0.5)
+    #allows for feedback
     receive_string = ser.readline().decode('utf-8').rstrip()
     print(receive_string)
