410c5d05404ebe488755c8905619dd174f61b22f
Alysanb <144813223+Alysanb@users.noreply.github.com>
2024-04-08 20:15:15 +1000

    Update Arduino to Pi.ino

--- Serial Communication/Arduino to Pi.ino
+++ Serial Communication/Arduino to Pi.ino
@@ -1,11 +1,8 @@
-int a =0; 
-
 void setup() {
 Serial.begin(9600);              //Starting serial communication
 }
   
-void loop() {
-  //a++;                          // convert a value to hexa 
+void loop() {                       // convert a value to hexa 
   Serial.println("Hello");   // send the data
   delay(1000);                  // give the loop some break
 }
